96622af3c8771a0ca6e49c79b9907da0d120ba5e
Snehap1011 <95018764+Snehap1011@users.noreply.github.com>
2021-11-27 17:12:26 +0530

    Add files via upload

---
+++ SalesMonitoringApp/Models/EmployeeRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesMonitoringApp.Models
+{
+    public partial class EmployeeRegistration
+    {
+        public EmployeeRegistration()
+        {
+            VisitTable = new HashSet<VisitTable>();
+        }
+
+        public int EmpId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? Age { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public decimal? PhoneNumber { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual TblUser User { get; set; }
+        public virtual ICollection<VisitTable> VisitTable { get; set; }
+    }
+}
---
+++ SalesMonitoringApp/Models/SalesMonitoringSystemDBContext.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SalesMonitoringApp.Models
+{
+    public partial class SalesMonitoringSystemDBContext : DbContext
+    {
+        public SalesMonitoringSystemDBContext()
+        {
+        }
+
+        public SalesMonitoringSystemDBContext(DbContextOptions<SalesMonitoringSystemDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeeRegistration> EmployeeRegistration { get; set; }
+        public virtual DbSet<TblRole> TblRole { get; set; }
+        public virtual DbSet<TblUser> TblUser { get; set; }
+        public virtual DbSet<VisitTable> VisitTable { get; set; }
+/*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=SNEHAP\\SQLEXPRESS; Initial Catalog=SalesMonitoringSystemDB; Integrated security=True");
+            }
+        }
+*/
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeRegistration>(entity =>
+            {
+                entity.HasKey(e => e.EmpId)
+                    .HasName("PK__Employee__263E2DD32DFC5C6E");
+
+                entity.Property(e => e.EmpId).HasColumnName("Emp_id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).HasColumnType("numeric(18, 0)");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.EmployeeRegistration)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__EmployeeR__UserI__3B75D760");
+            });
+
+            modelBuilder.Entity<TblRole>(entity =>
+            {
+                entity.HasKey(e => e.RoleId)
+                    .HasName("PK__TblRole__8AFACE1A5B417EBD");
+
+                entity.Property(e => e.RoleName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblUser>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__TblUser__1788CC4C306EE8F4");
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserPassword)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.TblUser)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK_LOGIN");
+            });
+
+            modelBuilder.Entity<VisitTable>(entity =>
+            {
+                entity.HasKey(e => e.VisitId)
+                    .HasName("PK__VisitTab__DB49A42803A1AF4D");
+
+                entity.Property(e => e.VisitId).HasColumnName("Visit_id");
+
+                entity.Property(e => e.ContactNo)
+                    .HasColumnName("Contact_No")
+                    .HasColumnType("numeric(18, 0)");
+
+                entity.Property(e => e.ContactPerson)
+                    .HasColumnName("Contact_Person")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CustName)
+                    .HasColumnName("Cust_Name")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpId).HasColumnName("Emp_id");
+
+                entity.Property(e => e.InterestProduct)
+                    .HasColumnName("Interest_Product")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.IsDeleted).HasColumnName("Is_Deleted");
+
+                entity.Property(e => e.IsDisabled).HasColumnName("Is_Disabled");
+
+                entity.Property(e => e.VisitDatetime)
+                    .HasColumnName("Visit_Datetime")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.VisitSubject)
+                    .HasColumnName("Visit_Subject")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Emp)
+                    .WithMany(p => p.VisitTable)
+                    .HasForeignKey(d => d.EmpId)
+                    .HasConstraintName("FK__VisitTabl__Emp_i__3E52440B");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
